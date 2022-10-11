0026cd3825814f76866da127682b3d8734ead468
Kevin <kamcak@hotmail.es>
2022-10-10 22:21:28 -0500

    packages created

--- app/src/main/java/com/riseup/riseup_users/view/MainActivity.kt
+++ app/src/main/java/com/riseup/riseup_users/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.riseup.riseup_users
+package com.riseup.riseup_users.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.riseup.riseup_users.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
