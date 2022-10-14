1488b718bf5b910e65fbd9823122af0fba8d420e
Xoten <alejo.co9@hotmail.com>
2022-10-13 19:36:42 -0500

    working in login screen

--- app/src/main/java/com/riseup/riseup_users/view/MainActivity.kt
+++ app/src/main/java/com/riseup/riseup_users/view/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.riseup.riseup_users.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
-import com.riseup.riseup_users.R
+import androidx.appcompat.app.AppCompatActivity
 import com.riseup.riseup_users.databinding.ActivityMainBinding
 import com.riseup.riseup_users.viewmodel.MainViewModel
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,5 +19,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        val switchActivityIntent = Intent(this, LoginActivity::class.java)
+        switchActivityIntent.putExtra(
+            "message",
+            "From: " + LoginActivity::class.java.getSimpleName()
+        )
+        startActivity(switchActivityIntent)
     }
 }
