21c654e870d72fd327bb559530afefe8f2b4ca0c
Xoten <alejo.co9@hotmail.com>
2022-10-14 16:44:40 -0500

    fixing

--- app/src/main/java/com/riseup/riseup_users/view/RegisterActivity.kt
+++ app/src/main/java/com/riseup/riseup_users/view/RegisterActivity.kt
@@ -2,10 +2,16 @@ package com.riseup.riseup_users.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.riseup.riseup_users.databinding.ActivityLoginBinding
+import com.riseup.riseup_users.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
