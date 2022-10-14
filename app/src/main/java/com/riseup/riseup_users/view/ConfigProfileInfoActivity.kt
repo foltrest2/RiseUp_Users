2a124d8459b23084eb011c5e34f553200db6cc00
Kevin <kamcak@hotmail.es>
2022-10-13 19:32:53 -0500

    config activity front ok

---
+++ app/src/main/java/com/riseup/riseup_users/view/ConfigProfileInfoActivity.kt
@@ -0,0 +1,11 @@
+package com.riseup.riseup_users.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConfigProfileInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_config_profile_info)
+    }
+}
