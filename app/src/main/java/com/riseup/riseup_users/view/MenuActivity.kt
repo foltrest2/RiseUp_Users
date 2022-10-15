f2e30fe367b635ff87802a1e1cd95ad3c8eb7cdf
Xoten <alejo.co9@hotmail.com>
2022-10-15 14:01:24 -0500

    working on nav menu

---
+++ app/src/main/java/com/riseup/riseup_users/view/MenuActivity.kt
@@ -0,0 +1,11 @@
+package com.riseup.riseup_users.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_menu)
+    }
+}
