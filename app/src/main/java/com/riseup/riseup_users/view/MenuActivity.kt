6a049bb719f470fdd3e448c293c7bfd3c4709b13
Xoten <alejo.co9@hotmail.com>
2022-10-15 21:16:14 -0500

    select payment screen finished, also menu bar is finished

--- app/src/main/java/com/riseup/riseup_users/view/MenuActivity.kt
+++ app/src/main/java/com/riseup/riseup_users/view/MenuActivity.kt
@@ -2,10 +2,39 @@ package com.riseup.riseup_users.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.riseup.riseup_users.PaymentSelectionFragment
+import com.riseup.riseup_users.R
+import com.riseup.riseup_users.databinding.ActivityMenuBinding
 
 class MenuActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMenuBinding
+    private lateinit var newPaymentSelectionFragment: PaymentSelectionFragment
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_menu)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        newPaymentSelectionFragment = PaymentSelectionFragment.newInstance()
+        showFragment(newPaymentSelectionFragment)
+
+        binding.menuApp.setOnItemSelectedListener { menuItem->
+            if(menuItem.itemId == R.id.homeOpMenu){
+            }else if(menuItem.itemId == R.id.carOpMenu){
+                showFragment(newPaymentSelectionFragment)
+
+            }else if(menuItem.itemId == R.id.configmOpMenu){
+            }
+            true
+        }
+
+    }
+    fun showFragment(fragment: Fragment){
+            val transaction = supportFragmentManager.beginTransaction()
+             transaction.replace(R.id.fragmentContainer, fragment)
+             transaction.commit()
     }
 }
