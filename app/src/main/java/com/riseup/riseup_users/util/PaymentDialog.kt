03e269aff4ea9a22687c2b64cfc0931fbdf6e7f6
Xoten <alejo.co9@hotmail.com>
2022-10-17 12:44:00 -0500

    credit card payment pop up is done

---
+++ app/src/main/java/com/riseup/riseup_users/util/PaymentDialog.kt
@@ -0,0 +1,39 @@
+package com.riseup.riseup_users.util
+
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import com.riseup.riseup_users.databinding.ShowCardsDialogBinding
+
+class PaymentDialog(
+    private val onSubmitClickListener: (String) -> Unit
+):DialogFragment() {
+
+    private lateinit var binding : ShowCardsDialogBinding
+
+    override fun onStart() {
+        super.onStart()
+        getDialog()!!.window!!
+            .setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+    }
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        binding = ShowCardsDialogBinding.inflate(LayoutInflater.from(context))
+
+        val builder = AlertDialog.Builder(requireActivity())
+        builder.setView(binding.root)
+
+
+        val dialog = builder.create()
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.window!!.setGravity(Gravity.BOTTOM)
+        dialog.window!!.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        return dialog
+    }
+}
