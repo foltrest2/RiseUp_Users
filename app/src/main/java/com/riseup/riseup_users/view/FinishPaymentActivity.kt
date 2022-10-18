2119a3276f0126c91357f8fccb9c9c7bd5ea9f9e
Xoten <alejo.co9@hotmail.com>
2022-10-18 01:31:03 -0500

    order Detail pop up is finished

--- app/src/main/java/com/riseup/riseup_users/view/FinishPaymentActivity.kt
+++ app/src/main/java/com/riseup/riseup_users/view/FinishPaymentActivity.kt
@@ -3,6 +3,7 @@ package com.riseup.riseup_users.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.riseup.riseup_users.databinding.FinishPaymentBinding
+import com.riseup.riseup_users.util.OrderDialog
 
 
 class FinishPaymentActivity : AppCompatActivity() {
@@ -11,5 +12,9 @@ class FinishPaymentActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding =  FinishPaymentBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.showPUOrderProducts.setOnClickListener {
+
+            OrderDialog().show(supportFragmentManager,"dialog")
+        }
     }
 }
