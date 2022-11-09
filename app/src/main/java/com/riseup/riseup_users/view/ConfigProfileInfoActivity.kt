187b0fc7b0ba3a4960e85c77559ef48719b9a926
Kevin <kamcak@hotmail.es>
2022-11-08 22:47:04 -0500

    Fixing pull

--- app/src/main/java/com/riseup/riseup_users/view/ConfigProfileInfoActivity.kt
+++ app/src/main/java/com/riseup/riseup_users/view/ConfigProfileInfoActivity.kt
@@ -11,6 +11,9 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.google.gson.Gson
 import com.riseup.riseup_users.databinding.ActivityConfigProfileInfoBinding
 import com.riseup.riseup_users.model.User
